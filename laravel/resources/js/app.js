2ed16309a5057c1420bc2b528ab41abce1679075
Adomoraes <adomoraes@gmail.com>
2025-11-15 17:58:19 -0300

    feat: Integração de componentes Vue.js
    - Adicionado registro de componentes Vue
    - Importação de Vue e CSS
    - Configuração do Vite para Vue 2

--- laravel/resources/js/app.js
+++ laravel/resources/js/app.js
@@ -1,1 +1,15 @@
-import './bootstrap';
+// Ficheiro: resources/js/app.js
+import "../css/app.css"
+import Vue from "vue"
+
+// --- ADICIONE ESTAS LINHAS ---
+// 1. Crie a pasta /components
+// 2. Registe o seu componente "pai"
+Vue.component("solicitacao-exames", () =>
+	import("./components/SolicitacaoExames.vue")
+)
+// -----------------------------
+
+const app = new Vue({
+	el: "#app",
+})
