f489d760faa293b41fc0e45d6f3c34639b7d3df2
Adomoraes <adomoraes@gmail.com>
2025-11-17 13:02:03 -0300

    feat(ui): Aprimoramento das ações da barra lateral e da tabela
    - Substituídos os links de texto por ícones
    - Removida a dependência do Heroicons
    - Adicionadas dicas de ferramentas para as ações
    - Alterado o título do aplicativo para Eyecare

--- laravel/resources/js/components/Sidebar.vue
+++ laravel/resources/js/components/Sidebar.vue
@@ -5,7 +5,7 @@
 		<div class="flex h-full flex-col overflow-y-auto bg-dark-accent p-4">
 			<div class="mb-6 flex items-center justify-between">
 				<span v-if="!isRetracted" class="text-2xl font-bold text-white">
-					Exames
+					Eyecare
 				</span>
 				<button @click="toggle" class="text-light-bg hover:text-white p-2">
 					<svg
@@ -35,21 +35,10 @@
 						<a
 							:href="href"
 							@click="navigate"
-							class="flex items-center p-3 text-light-bg">
-							<span class="h-6 w-6">
-								<svg
-									xmlns="http://www.w3.org/2000/svg"
-									fill="none"
-									viewBox="0 0 24 24"
-									stroke-width="1.5"
-									stroke="currentColor"
-									class="h-6 w-6">
-									<path
-										stroke-linecap="round"
-										stroke-linejoin="round"
-										d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-								</svg>
-							</span>
+							class="flex items-center p-3 text-light-bg"
+							:title="link.name">
+							<span class="h-6 w-6" v-html="link.iconSvg"></span>
+
 							<span v-if="!isRetracted" class="ml-3">{{ link.name }}</span>
 						</a>
 					</li>
@@ -60,10 +49,12 @@
 </template>
 
 <script>
+// (3) REMOVEMOS TODOS OS IMPORTS DE '@heroicons/vue'
+
 export default {
 	name: "Sidebar",
+	// (4) REMOVEMOS O 'components: { ... }'
 	props: {
-		// O 'pai' (Layout) vai dizer-nos se estamos retraídos
 		isRetracted: {
 			type: Boolean,
 			default: false,
@@ -71,17 +62,36 @@ export default {
 	},
 	data() {
 		return {
-			// Lista de páginas do nosso dashboard
+			// (5) Adicionamos a propriedade 'iconSvg' com o código SVG
 			links: [
-				{ name: "Dashboard", path: "/" },
-				{ name: "Gestão de Exames", path: "/exames" },
-				{ name: "Gestão de Pacotes", path: "/pacotes" },
-				{ name: "Solicitar Exames", path: "/solicitacao" },
+				{
+					name: "Dashboard",
+					path: "/",
+					iconSvg:
+						'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" /></svg>',
+				},
+				{
+					name: "Gestão de Exames",
+					path: "/exames",
+					iconSvg:
+						'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m0 12.75h7.5m-7.5 3H12M10.5 2.25H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z" /></svg>',
+				},
+				{
+					name: "Gestão de Pacotes",
+					path: "/pacotes",
+					iconSvg:
+						'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M20.25 7.5l-.625 10.632a2.25 2.25 0 01-2.247 2.118H6.622a2.25 2.25 0 01-2.247-2.118L3.75 7.5M10.5 11.25h3M12 15h.008m-7.008 0h14.016m-5.25 3.75h.008m-3.75 0h.008m-3.75 0h.008m0 0h.008m-3.75 0h.008m-3.75 0h.008M12 3.75h.008m-3.75 0h.008m-3.75 0h.008m0 0h.008m-3.75 0h.008m-3.75 0h.008m-3.75 0H12m0 0h.008m-3.75 0h.008m-3.75 0h.008m0 0h.008m-3.75 0h.008m-3.75 0h.008" /></svg>',
+				},
+				{
+					name: "Solicitar Exames",
+					path: "/solicitacao",
+					iconSvg:
+						'<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m.75 12l3 3m0 0l3-3m-3 3v-6m-1.5 6H5.625a1.125 1.125 0 01-1.125-1.125V6.75A1.125 1.125 0 015.625 5.625h3.375c.621 0 1.125.504 1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125h1.5a3.375 3.375 0 003.375-3.375V7.125A1.125 1.125 0 0118.375 6h.223a1.125 1.125 0 011.125 1.125v13.5A1.125 1.125 0 0118.375 21.75H5.625a1.125 1.125 0 01-1.125-1.125v-1.5c0-.621.504-1.125 1.125-1.125H8.25" /></svg>',
+				},
 			],
 		}
 	},
 	methods: {
-		// Quando o botão for clicado, avisamos o 'pai'
 		toggle() {
 			this.$emit("toggle")
 		},
--- laravel/resources/js/views/GestaoPacotes.vue
+++ laravel/resources/js/views/GestaoPacotes.vue
@@ -40,13 +40,37 @@
 						<td class="p-4">
 							<button
 								@click="abrirModalParaEditar(pacote)"
-								class="text-blue-600 hover:text-blue-900 font-medium mr-3">
-								Editar
+								class="text-blue-600 hover:text-blue-900 p-1 rounded-full hover:bg-light-bg"
+								title="Editar Pacote">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-5 w-5">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L6.832 19.82a4.5 4.5 0 01-1.897 1.13l-2.685.8.8-2.685a4.5 4.5 0 011.13-1.897L16.863 4.487zm0 0L19.5 7.125" />
+								</svg>
 							</button>
 							<button
 								@click="handleDelete(pacote.id)"
-								class="text-red-600 hover:text-red-900 font-medium">
-								Excluir
+								class="text-red-600 hover:text-red-900 p-1 rounded-full hover:bg-red-100 ml-2"
+								title="Excluir Pacote">
+								<svg
+									xmlns="http://www.w3.org/2000/svg"
+									fill="none"
+									viewBox="0 0 24 24"
+									stroke-width="1.5"
+									stroke="currentColor"
+									class="h-5 w-5">
+									<path
+										stroke-linecap="round"
+										stroke-linejoin="round"
+										d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12.54 0c-.27-0.091-.54-0.18-.81-0.27m-.81.27L3 5.79m0 0c0-0.407.328-0.735.735-0.735h18.53c.407 0 .735.328.735.735m-19.995 0l-1.17 1.17m1.17-1.17h19.995" />
+								</svg>
 							</button>
 						</td>
 					</tr>
